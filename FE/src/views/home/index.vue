7e0f4c4c064eb37bbf53bf43d74a816e684665ff
gxy <1164441248@qq.com>
2020-06-03 22:29:21 +0800

    初始化搜索模块

---
+++ FE/src/views/home/index.vue
@@ -0,0 +1,16 @@
+<template>
+<top></top>
+</template>
+<script>
+import Top from '../../components/commom/top'
+export default {
+  components: {
+    top: Top
+  }
+
+}
+</script>
+
+<style>
+
+</style>
